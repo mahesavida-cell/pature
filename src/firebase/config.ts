8255f1585ca63ed10741f4e5397880d7e6ad71d3
Oppo Ida <oppoida128@gmail.com>
2026-05-05 03:11:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5202406818-6e9f9",
+  "appId": "1:403799460197:web:a69f1553cd74afb10b798c",
+  "apiKey": "AIzaSyB6mIg8iYRFfbUefj0x017Qvbh2_I_jqKA",
+  "authDomain": "studio-5202406818-6e9f9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "403799460197"
+};
