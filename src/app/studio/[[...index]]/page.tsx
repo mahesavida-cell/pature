c50d60a0427b2abc5f1c104ad732536e45b6278c
Oppo Ida <oppoida128@gmail.com>
2026-05-06 07:21:48 +0000

    ## Error Type Console Error
    ## Error Message
    React does not recognize t

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,13 +1,18 @@
-
 'use client';
 
 /**
  * Halaman ini merender Sanity Studio secara penuh.
- * Rute ini bersifat dinamis agar Sanity UI dapat menangani routing internal.
+ * Menggunakan dynamic import dengan ssr: false untuk menghindari masalah hidrasi
+ * dan kebocoran prop internal yang tidak dikenali oleh React 19.
  */
 
-import {NextStudio} from 'next-sanity/studio';
+import dynamic from 'next/dynamic';
 import config from '../../../../sanity.config';
+
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
